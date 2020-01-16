702180b5ecbc1c6cd417af5ed8c4e02598770ea3
Ruben Padilla <rubbenpad2@gmail.com>
2020-01-16 12:15:57 -0500

    now-webpack-initial-config

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const container = document.getElementById('app')
+
+const hola = <h1>Seguimos adelate con el proyecto!</h1>
+
+ReactDOM.render(hola, container)
---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+  output: {
+    filename: 'app.bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react'],
+          },
+        },
+      },
+    ],
+  },
+  plugins: [new HtmlWebpackPlugin({ template: 'src/index.html' })],
+}
