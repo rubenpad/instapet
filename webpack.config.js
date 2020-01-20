ff77b0a24aa88f52e46cf82dd129f9f49f0ed7a9
Ruben Padilla <rubbenpad2@gmail.com>
2020-01-19 22:18:31 -0500

    pwa

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,7 @@
+const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+const WebpackPWAManifest = require('webpack-pwa-manifest')
+const WorkboxWebpackPlugin = require('workbox-webpack-plugin')
 
 module.exports = {
   output: {
@@ -23,5 +26,38 @@ module.exports = {
       }
     ]
   },
-  plugins: [new HtmlWebpackPlugin({ template: 'src/index.html' })]
+  plugins: [
+    new HtmlWebpackPlugin({ template: 'src/index.html' }),
+    new WebpackPWAManifest({
+      name: 'Petgram | Your app to see pet photos',
+      short_name: 'Petgram 🐶',
+      description: 'You can see pet photos',
+      background_color: '#fff',
+      theme_color: '#b1a',
+      icons: [
+        {
+          src: path.resolve('src/assets/icon.png'),
+          sizes: [96, 128, 192, 256, 384, 512]
+        }
+      ]
+    }),
+    new WorkboxWebpackPlugin.GenerateSW({
+      runtimeCaching: [
+        {
+          urlPattern: new RegExp('https://(res.cloudinary|images.unplash).com'),
+          handler: 'CacheFirst',
+          options: {
+            cacheName: 'images'
+          }
+        },
+        {
+          urlPattern: new RegExp('https://instapet-api.rubbenpad2.now.sh'),
+          handler: 'NetworkFirst',
+          options: {
+            cacheName: 'api'
+          }
+        }
+      ]
+    })
+  ]
 }
